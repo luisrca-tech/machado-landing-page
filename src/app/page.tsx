521ec07043643504ce7efe1ba66fa10f8441847c
Luis Felipe <luisrochacruzalves@gmail.com>
2025-04-25 10:07:01 -0300

    ✨ Feat: integrate Radix UI components, enhance Header with mobile navigation, and update layout structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ import { Container } from "~/components/ui/Container";
 export default function HomePage() {
 	return (
 		<main>
-			<Container>
-				<HeroSection />
-			</Container>
+			<section className="pt-14 pb-10 lg:pt-32 lg:pb-20">
+				<Container>
+					<HeroSection />
+				</Container>
+			</section>
 		</main>
 	);
 }
