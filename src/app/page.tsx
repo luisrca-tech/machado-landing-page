bd344027403ee646d3962b7a611560c5f65a2c92
Luis Felipe <luisrochacruzalves@gmail.com>
2025-04-28 10:39:00 -0300

    ✨ Feat: add WhoWeAre section to HomePage and include office background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { Acting } from "~/components/section/Acting";
 import { HeroSection } from "~/components/section/Hero";
 import { OurBenefits } from "~/components/section/OurBenefits";
+import { WhoWeAre } from "~/components/section/WhoWeAre";
 import { Container } from "~/components/ui/Container";
+import Office from "public/images/officePhoto.jpg";
 
 export default function HomePage() {
   return (
@@ -20,6 +22,21 @@ export default function HomePage() {
         <Container>
           <Acting />
         </Container>
+      </section>
+      <section className="py-10 lg:py-20 relative overflow-hidden">
+        <div className="absolute inset-0 w-full h-full z-0">
+          <img
+            src={Office.src}
+            alt="Escritório Machado Advogados"
+            className="w-full h-full object-cover object-center blur-sm brightness-75"
+            draggable={false}
+          />
+        </div>
+        <div className="relative z-10">
+          <Container>
+            <WhoWeAre />
+          </Container>
+        </div>
       </section>
     </main>
   );
