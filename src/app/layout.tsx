9306f519be1ba523655eb9dc9489d3d22f5500d9
Luis Felipe <luisrochacruzalves@gmail.com>
2025-04-30 12:16:08 -0300

    🚨 Fix: run linter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,32 +6,29 @@ import { montserrat } from "~/assets/fonts/montserrat";
 import { Footer } from "~/components/ui/Footer";
 
 export const metadata: Metadata = {
-  title: "Machado Advogados",
-  description: "Advogados Machado",
-  icons: [{ rel: "icon", url: "/favicon.png" }],
+	title: "Machado Advogados",
+	description: "Advogados Machado",
+	icons: [{ rel: "icon", url: "/favicon.png" }],
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html
-      lang="en"
-      className={`${montserrat.className}`}
-    >
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-        />
-      </head>
-      <body className="bg-[#f9f9f9]">
-        {/* <Header /> */}
-        {children}
-        <div className="bg-[#0f1b2b]">
-          <Footer />
-        </div>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className={`${montserrat.className}`}>
+			<head>
+				<link
+					rel="stylesheet"
+					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+				/>
+			</head>
+			<body className="bg-[#f9f9f9]">
+				{/* <Header /> */}
+				{children}
+				<div className="bg-[#0f1b2b]">
+					<Footer />
+				</div>
+			</body>
+		</html>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,42 +8,36 @@ import { WhoWeAre } from "~/components/section/WhoWeAre";
 import { Container } from "~/components/ui/Container";
 
 export default function HomePage() {
-  return (
-    <main>
-      <section className="relative bg-[#0f1b2b]">
-        <Container className="z-10 px-0 py-8">
-          <HeroSection />
-        </Container>
-      </section>
-      <section className="py-10 lg:py-16">
-        <Container>
-          <OurBenefits />
-        </Container>
-      </section>
-      <section
-        className="py-10 lg:py-20 bg-[#0f1b2b]"
-        id="acting"
-      >
-        <Container>
-          <Acting />
-        </Container>
-      </section>
-      <section className=" pt-10 lg:py-20">
-        <Container>
-          <WhoWeAre />
-        </Container>
-      </section>
-      <section className="bg-[#F9FAFB] pb-10 md:py-20">
-        <WhatTheySay />
-      </section>
-      <section
-        className="py-10 lg:py-20"
-        id="faq"
-      >
-        <Container>
-          <Faq />
-        </Container>
-      </section>
-    </main>
-  );
+	return (
+		<main>
+			<section className="relative bg-[#0f1b2b]">
+				<Container className="z-10 px-0 py-8">
+					<HeroSection />
+				</Container>
+			</section>
+			<section className="py-10 lg:py-16">
+				<Container>
+					<OurBenefits />
+				</Container>
+			</section>
+			<section className="bg-[#0f1b2b] py-10 lg:py-20" id="acting">
+				<Container>
+					<Acting />
+				</Container>
+			</section>
+			<section className="bg-white/90 pt-10 lg:py-20">
+				<Container>
+					<WhoWeAre />
+				</Container>
+			</section>
+			<section className="pb-10 md:py-20">
+				<WhatTheySay />
+			</section>
+			<section className="bg-white/90 py-10 lg:py-20">
+				<Container>
+					<Faq />
+				</Container>
+			</section>
+		</main>
+	);
 }
